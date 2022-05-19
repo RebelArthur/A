f28ca061cf3fa0e56ba8dece7ec2d7ad5dac0477
RebelArthur <61909704+RebelArthur@users.noreply.github.com>
2022-05-19 15:32:52 +0300

    Add files via upload
    I might just make an alarm as well...

---
+++ Studying.py
@@ -0,0 +1,10 @@
+import random
+lessons = ['Math', 'Literature', 'Chemistry', 'Physics', 'Biology', 'History', 'Geography', 'German', 'Islam Religion', 'Islam Religion2']
+lesson = random.choice(lessons)
+period = random.randint(40, 75)
+if lesson in ('Islam Religion2' or 'Islam Religion'):
+    period = random.randint(15,40)
+    print(lesson, "," , period, "minutes")
+
+else:
+    print(lesson, "," , period, "minutes")

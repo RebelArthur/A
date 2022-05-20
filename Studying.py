bae491f2634068235abce9add9b7512ba7a3a3c8
RebelArthur <61909704+RebelArthur@users.noreply.github.com>
2022-05-20 23:17:31 +0300

    Update Studying.py
    Made the studying period shorter because I'm way too lazy to just study that much lol

--- Studying.py
+++ Studying.py
@@ -1,7 +1,7 @@
 import random
 lessons = ['Math', 'Literature', 'Chemistry', 'Physics', 'Biology', 'History', 'Geography', 'German', 'Islam Religion', 'Islam Religion2']
 lesson = random.choice(lessons)
-period = random.randint(40, 75)
+period = random.randint(15, 50)
 if lesson in ('Islam Religion2' or 'Islam Religion'):
     period = random.randint(15,40)
     print(lesson, "," , period, "minutes")

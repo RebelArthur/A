74ef14b43823d860c99ed94445b632f804f39f51
RebelArthur <61909704+RebelArthur@users.noreply.github.com>
2022-05-21 22:51:20 +0300

    Update Studying.py
    thats way too much studying for me so i just lowered the periods

--- Studying.py
+++ Studying.py
@@ -6,10 +6,10 @@ lessons = ['Math', 'Literature', 'Chemistry', 'Physics', 'Biology',
            'Islam Religion2', 'Health and Traffic Culture']
 
 chosenlesson = random.choice(lessons)
-period = random.randint(15, 50)
+period = random.randint(5, 40)
 
 if chosenlesson in ('Islam Religion2' or 'Islam Religion'):
-    period = random.randint(15,40)
+    period = random.randint(8,25)
     print(chosenlesson, "," , period, "minutes")
 else:
     print(chosenlesson, "," , period, "minutes")

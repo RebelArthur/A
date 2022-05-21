3c5f92065d8afbb40e8788cddeffad1e3c54b038
RebelArthur <61909704+RebelArthur@users.noreply.github.com>
2022-05-21 13:16:20 +0300

    Update Studying.py
    More readable and also a waiting command

--- Studying.py
+++ Studying.py
@@ -1,11 +1,19 @@
+import time
 import random
-lessons = ['Math', 'Literature', 'Chemistry', 'Physics', 'Biology', 'History', 'Geography', 'German', 'Islam Religion', 'Islam Religion2', 'Health and Traffic Culture']
-lesson = random.choice(lessons)
-period = random.randint(15, 50)
-if lesson in ('Islam Religion2' or 'Islam Religion'):
-    period = random.randint(15,40)
-    print(lesson, "," , period, "minutes")
 
+lessons = ['Math', 'Literature', 'Chemistry', 'Physics', 'Biology',
+           'History','Geography', 'German','Islam Religion',
+           'Islam Religion2', 'Health and Traffic Culture']
+
+chosenlesson = random.choice(lessons)
+period = random.randint(15, 50)
+
+if chosenlesson in ('Islam Religion2' or 'Islam Religion'):
+    period = random.randint(15,40)
+    print(chosenlesson, "," , period, "minutes")
 else:
-    print(lesson, "," , period, "minutes")
+    print(chosenlesson, "," , period, "minutes")
+
+time.sleep(3.20)
+
 input("Press ENTER to exit.")
